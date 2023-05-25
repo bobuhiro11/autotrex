f47c04c2443ac18fafc61cd93b673ec265183e12
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-05-25 01:57:21 +0000

    Add an initial test.
    * Set low_end mode for testing.
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- serve.sh
+++ serve.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+set -x
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ip link add dut-p0 type veth peer name dut-p1
 ip link set up dut-p0
 ip link set up dut-p1
 
 cd $TREX_CORE
+cat $SCRIPTPATH/trex_cfg.yaml
 ./t-rex-64 -i --cfg $SCRIPTPATH/trex_cfg.yaml
