2b9c57a7c603d530f416405bf2e736125154151f
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-05-25 01:44:04 +0000

    Fix *.py files to pass lint.
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- benchmark.py
+++ benchmark.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
-import sys
-
-sys.path.append(
-    "/tmp/v3.02/automation/trex_control_plane" \
-    + "/interactive/trex/examples/stl"
-)
+import time
+import matplotlib.pyplot as plt
+from trex.stl.api import STLProfile, STLClient
 
 # FIXME: pkt -> eth
 
 # I don't know why but if pkt is small, then duration is ignored.
 DURATION = 1
 
-import stl_path
-from trex.stl.api import *
-from trex.utils.text_opts import format_text
-import matplotlib.pyplot as plt
-import time
 
 # e.g. pkg_size = 64 - 1518
+
 def single_run(c, profile, mult, pkt_size):
     start_at = time.time()
 
-    c.reset(ports = [0, 1])
+    c.reset(ports=[0, 1])
     c.remove_all_streams()
-    c.add_streams(profile.get_streams(), ports = [0, 1])
+    c.add_streams(profile.get_streams(), ports=[0, 1])
 
-    c.start(ports = [0], mult = str(mult) + "%", duration = DURATION)
-    c.wait_on_traffic(ports = [0, 1])
+    c.start(ports=[0], mult=str(mult) + "%", duration=DURATION)
+    c.wait_on_traffic(ports=[0, 1])
     s = c.get_stats()
 
     total = s["total"]
@@ -35,7 +28,7 @@ def single_run(c, profile, mult, pkt_size):
     bps = total["tx_bps"]
     pps = total["tx_pps"]
 
-    lossrate = max(0.0, (1 - rx_pkts / tx_pkts)) * 100.0 # percent
+    lossrate = max(0.0, (1 - rx_pkts / tx_pkts)) * 100.0  # percent
     success = lossrate <= 0.01
 
     end_at = time.time()
@@ -57,11 +50,11 @@ def single_run(c, profile, mult, pkt_size):
 
 
 def run(c, pkt_size):
-    profile = STLProfile.load_py("./tcp_1pkt.py", pkt_size = pkt_size)
+    profile = STLProfile.load_py("./tcp_1pkt.py", pkt_size=pkt_size)
 
     ok = 1
     ng = 101
-    for k in range(7): # 7 is enough to cover [1-100] mult.
+    for k in range(7):  # 7 is enough to cover [1-100] mult.
         if ok == ng - 1:
             break
 
@@ -78,51 +71,54 @@ def run(c, pkt_size):
 
 
 def plot_Gbps(packet_sizes, Gbpss, link_speed_bps):
-   X = packet_sizes
-   Y = Gbpss
-   Y_max = []
+    X = packet_sizes
+    Y = Gbpss
+    Y_max = []
 
-   # Calc theoretical maximum throuput.
-   for pkt_size in packet_sizes:
-       n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8) # include preameble(8Bytes)+inter frame gap(12Bytes)
-       Y_max.append(n_pkts * pkt_size * 8 / 1e9) # convert unit from bytes to Gbits.
+    # Calc theoretical maximum throuput.
+    for pkt_size in packet_sizes:
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8)
+        # convert unit from bytes to Gbits.
+        Y_max.append(n_pkts * pkt_size * 8 / 1e9)
 
-   plt.clf()
-   plt.style.use('seaborn-whitegrid')
-   plt.xlabel("Packet size (Bytes)")
-   plt.ylabel("Throuput (Gbps)")
-   plt.xticks(X)
-   plt.ticklabel_format(style='plain')
-   plt.plot(X, Y_max, marker=".", label="Theoretical", color="0.8")
-   plt.plot(X, Y, marker=".", label="Actual", linewidth=3)
-   plt.legend()
-   plt.savefig("Gbps.png")
+    plt.clf()
+    plt.style.use('seaborn-whitegrid')
+    plt.xlabel("Packet size (Bytes)")
+    plt.ylabel("Throuput (Gbps)")
+    plt.xticks(X)
+    plt.ticklabel_format(style='plain')
+    plt.plot(X, Y_max, marker=".", label="Theoretical", color="0.8")
+    plt.plot(X, Y, marker=".", label="Actual", linewidth=3)
+    plt.legend()
+    plt.savefig("Gbps.png")
 
 
 def plot_Mpps(packet_sizes, Mppss, link_speed_bps):
-   X = packet_sizes
-   Y = Mppss
-   Y_max = []
+    X = packet_sizes
+    Y = Mppss
+    Y_max = []
 
-   # Calc theoretical maximum throuput.
-   for pkt_size in packet_sizes:
-       n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8) # include preameble(8Bytes)+inter frame gap(12Bytes)
-       Y_max.append(n_pkts / 1e6)
+    # Calc theoretical maximum throuput.
+    for pkt_size in packet_sizes:
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8)
+        Y_max.append(n_pkts / 1e6)
 
-   plt.clf()
-   plt.style.use('seaborn-whitegrid')
-   plt.xlabel("Packet size (Bytes)")
-   plt.ylabel("Packet rate (Mpps)")
-   plt.xticks(X)
-   plt.ticklabel_format(style='plain')
-   plt.plot(X, Y_max, marker=".", label="Theoretical", color="0.8")
-   plt.plot(X, Y, marker=".", label="Actual", linewidth=3)
-   plt.legend()
-   plt.savefig("Mpps.png")
+    plt.clf()
+    plt.style.use('seaborn-whitegrid')
+    plt.xlabel("Packet size (Bytes)")
+    plt.ylabel("Packet rate (Mpps)")
+    plt.xticks(X)
+    plt.ticklabel_format(style='plain')
+    plt.plot(X, Y_max, marker=".", label="Theoretical", color="0.8")
+    plt.plot(X, Y, marker=".", label="Actual", linewidth=3)
+    plt.legend()
+    plt.savefig("Mpps.png")
 
 
 def main():
-    c = STLClient(verbose_level = 'error')
+    c = STLClient(verbose_level='error')
     c.connect()
 
     link_speed_bps = c.ports[0].get_speed_bps()
@@ -140,13 +136,13 @@ def main():
     finally:
         c.disconnect()
 
-
     Gbpss = []
     Mppss = []
     print("pkt_size", "bps", "pps")
     for pkt_size, result in results.items():
         print(pkt_size, result['bps'], result['pps'])
-        Gbpss.append(result['bps'] / 1e9) # Not use 2^N. use 10^N in networking.
+        # Not use 2^N. use 10^N in networking.
+        Gbpss.append(result['bps'] / 1e9)
         Mppss.append(result['pps'] / 1e6)
 
     plot_Gbps(pkt_sizes, Gbpss, link_speed_bps)
--- tcp_1pkt.py
+++ tcp_1pkt.py
@@ -1,32 +1,28 @@
-from trex_stl_lib.api import *
-import argparse
+from trex_stl_lib.api \
+        import Ether, IP, TCP, STLStream, STLTXCont, STLPktBuilder
 
-class UDP_1STREAM(object):
 
-    def create_stream (self, pkt_size):
+class TCP_1PKT(object):
+
+    def create_stream(self, pkt_size):
         payload_size = pkt_size
         payload_size -= 4  # FCS
-        payload_size -= 20 # TCP
-        payload_size -= 20 # IPv4
-        payload_size -= 14 # ETH
+        payload_size -= 20  # TCP
+        payload_size -= 20  # IPv4
+        payload_size -= 14  # ETH
 
         return STLStream(
-            packet =
-                    STLPktBuilder(
-                        pkt = Ether()/IP(src="16.0.0.1",dst="48.0.0.1")/
-                                TCP(dport=12,sport=1025)/(payload_size*'x')
-                    ),
-             mode = STLTXCont())
+            packet=STLPktBuilder(
+                pkt=Ether()/IP(src="16.0.0.1", dst="48.0.0.1") /
+                TCP(dport=12, sport=1025)/(payload_size*'x')
+            ),
+            mode=STLTXCont())
 
     # e.g. pkt_size = 64 - 1518
-    def get_streams (self, pkt_size=64, **kwargs):
-        # parser = argparse.ArgumentParser(description='Argparser for {}'.format(os.path.basename(__file__)),
-                                         # formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        # args = parser.parse_args(tunables)
-        # create 1 stream
-        return [ self.create_stream(pkt_size) ]
+    def get_streams(self, pkt_size=64, **kwargs):
+        return [self.create_stream(pkt_size)]
 
 
 # dynamic load - used for trex console or simulator
 def register():
-    return UDP_1STREAM()
+    return TCP_1PKT()
