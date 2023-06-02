5e5da2170c32b41576cc7a2276ba1189bb34fbb5
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-06-02 02:09:47 +0000

    Fix output from ./simulate.sh.
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- simulate.sh
+++ simulate.sh
@@ -4,6 +4,6 @@
 rm -f /tmp/a.pcap
 
 (cd $TREX_CORE; ./stl-sim -f $SCRIPTPATH/autotrex/$TREX_INPUT_FILE \
-               -o /tmp/a.pcap)
+               -o /tmp/a.pcap > $SCRIPTPATH/stl-sim.log 2>&1)
 
-tcpdump -r /tmp/a.pcap -nnn
+tcpdump -r /tmp/a.pcap -nnn -c 10 -t
